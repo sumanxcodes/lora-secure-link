bb3fd920b113d1d0df71c4470f60362908ac2a69
Suman Raj Sharma <sumanrajsharma@Sumans-MacBook-Pro.local>
2025-04-18 11:15:56 +1000

    feat: added simple lora_rx_node and lora_tx_node files
    - Created basic receiver and transmitter files using LoRaConfig and LoRaSetup
    - Updated platformio.ini to use these new files
    - Deleted default main.cpp from project

---
+++ src/lora_rx_node.cpp
@@ -0,0 +1,32 @@
+// lora_rx_node.cpp
+#include <Arduino.h>
+#include <LoRa.h>
+#include "LoRaConfig.h"
+#include "LoRaSetup.h"
+
+void setup()
+{
+    Serial.begin(9600);
+    while (!Serial)
+        ;
+
+    if (!setupLoRa())
+    {
+        Serial.println("LoRa init failed");
+        while (1)
+            ;
+    }
+
+    Serial.println("[RX] Central receiver ready.");
+}
+
+void loop()
+{
+    if (LoRa.parsePacket())
+    {
+        String msg = "";
+        while (LoRa.available())
+            msg += (char)LoRa.read();
+        Serial.println("Received: " + msg);
+    }
+}
